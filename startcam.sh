9da02f6213be6528e3d9931ff40ce68570e1cebd
Siggi Bjarnason <siggib007@users.noreply.github.com>
2025-02-20 14:00:47 +0000

    fix

--- startcam.sh
+++ startcam.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+#exec > /home/siggib/prusacam/cam.log 2>&1
+#set -x
+
 export PRUSATOKEN=Pf3Wid2WOjGjaWRFs30c
 export CAMFP=db019aca-826a-4211-bb8d-5481ed352e07
 export CAMPIC=/tmp/prusaimg.jpg
 export CAMINT=5
 export SILENT=true
-nohup python3 /home/siggib/prusacam/prusacam.py &
-nohup python3 /home/siggib/prusacam/tempmon.py --sleep 120 --silent &
+python3 /home/siggib/prusacam/prusacam.py
+# python3 /home/siggib/prusacam/tempmon.py --sleep 120 --silent
