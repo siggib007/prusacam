67af67b3e95766c6988aac67a31ca322b1d35621
Siggi Bjarnason <siggib007@users.noreply.github.com>
2026-03-23 22:44:17 +0000

    Betterstack log option

--- tempmon.py
+++ tempmon.py
@@ -16,6 +16,54 @@ import time
 from vcgencmd import Vcgencmd
 import argparse
 import inputimeout
+import requests
+import json
+
+# Define few Defaults
+iLogLevel = 4  # How much logging should be done. Level 10 is debug level, 0 is none
+iTimeOut = 180  # Max time in seconds to wait for network response
+iMinQuiet = 2  # Minimum time in seconds between API calls
+strURL = "https://s2313682.eu-fsn-3.betterstackdata.com/metrics"
+
+if sys.version_info[0] > 2:
+    import urllib.parse as urlparse
+    # The following line surpresses a warning that we aren't validating the HTTPS certificate
+    requests.urllib3.disable_warnings()
+else:
+   print("This script is only supported on python 3")
+   sys.exit(9)
+
+def isInt(CheckValue):
+    """
+    function to safely check if a value can be interpreded as an int
+    Parameter:
+      Value: A object to be evaluated
+    Returns:
+      Boolean indicating if the object is an integer or not.
+    """
+    if isinstance(CheckValue, (float, int, str)):
+        try:
+            fTemp = int(CheckValue)
+        except ValueError:
+            fTemp = "NULL"
+    else:
+        fTemp = "NULL"
+    return fTemp != "NULL"
+
+def FetchEnv(strVarName):
+  """
+  Function that fetches the specified content of specified environment variable,
+  converting nonetype to empty string.
+  Parameters:
+    strVarName: The name of the environment variable to be fetched
+  Returns:
+    The content of the environment or empty string
+  """
+
+  if os.getenv(strVarName) != "" and os.getenv(strVarName) is not None:
+    return os.getenv(strVarName)
+  else:
+    return ""
 
 def timed_input(prompt, timeout):
     try:
@@ -40,7 +88,48 @@ def isInt(CheckValue):
         fTemp = "NULL"
     return fTemp != "NULL"
 
+def Convert2OpenMetricGauge(dictPayloads):
+    """
+    Transform a dictionary of metrics into OpenMetrics format, type Gauge.
+
+    Parameters:
+      dictPayloads: Dictionary with metric names as keys and numerical values
+
+    Returns:
+      List of dictionaries in OpenMetrics format
+
+    Example:
+      >>> BuildMetricsPayload({"temperature": 45.2, "clock_speed": 1800})
+      [
+          {"name": "temperature", "gauge": {"value": 45.2}},
+          {"name": "clock_speed", "gauge": {"value": 1800}}
+      ]
+    """
+    listMetrics = []
+    for metric_name, metric_value in dictPayloads.items():
+        dictMetric = {
+            "name": metric_name,
+            "gauge": {
+                "value": metric_value
+            }
+        }
+        listMetrics.append(dictMetric)
+    return listMetrics
+
+def SubmitMetric(dictPayload):
+  strMethod = "post"
+
+  print("Submitting metric to server:{}".format(json.dumps(dictPayload)))
+  dictHeader = {}
+  dictHeader["Content-type"] = "application/json"
+  dictHeader["Authorization"] = "Bearer " + strToken
+  WebRequest = requests.request(strMethod, strURL, json=dictPayload, timeout=iTimeOut, headers=dictHeader, verify=False)
+
+  return WebRequest
+
 def main():
+  global strToken
+
 
   objParser = argparse.ArgumentParser(description="Raspberry Pi Monitor")
   objParser.add_argument("--silent", dest="silent",
@@ -71,6 +160,13 @@ def main():
   if objArgs.file_name is not None:
      strFilePath=objArgs.file_name
 
+  # fetching secrets in environment
+  strToken = FetchEnv("TOKEN")
+  if strToken == "":
+    print("No API token, can't post without it.")
+    sys.exit(9)
+
+
   if not objArgs.silent:
     print("This is a script to raspberrypi cpu stats. "
             "This is running under Python Version {}".format(strVersion))
@@ -88,10 +184,17 @@ def main():
     fTempiture = objvcgm.measure_temp()
     iClockSpeed = int(objvcgm.measure_clock("arm")/1e6)
     bThrottled = objvcgm.get_throttled()["breakdown"]["2"]
+    dictPayload = {}
+    dictPayload["temperature"] = fTempiture
+    dictPayload["clock_speed"] = iClockSpeed
+    dictPayload["throttled"] = bThrottled
+    lstMetrics = Convert2OpenMetricGauge(dictPayload)
+    WebResponse = SubmitMetric(lstMetrics)
 
     strOut = "{},{},{},{}\n".format(strCurTime,fTempiture,iClockSpeed,bThrottled)
     if not objArgs.silent:
       print(strOut, end="")
+      print("Response from server: {} {}".format(WebResponse.status_code, WebResponse.text))
     objFile.write(strOut)
     objFile.flush()
     if objArgs.silent:
